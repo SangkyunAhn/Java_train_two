76251bb51bc7571c80bda212efe005df4678855b
Sangkyun Ahn <sangkyun4789@naver.com>
2021-07-28 20:09:48 +0900

    12~15 완료

---
+++ two/src/chap15/six/one/Coin.java
@@ -0,0 +1,15 @@
+package chap15.six.one;
+
+public class Coin {
+	
+	private int value;
+	
+	public Coin(int value) {
+		this.value = value;
+	}
+	
+	public int getValue() {
+		return value;
+	}
+
+}
---
+++ two/src/chap15/six/two/Message.java
@@ -0,0 +1,13 @@
+package chap15.six.two;
+
+public class Message {
+	
+	public String command;
+	public String to;
+	
+	public Message(String command, String to) {
+		this.command = command;
+		this.to = to;
+	}
+
+}
